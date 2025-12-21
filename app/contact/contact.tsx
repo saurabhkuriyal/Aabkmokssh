df32a7a1ec660aedf0ebfb013be9b385f812221f
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-12-21 23:22:36 +0530

    formatting contact  page

--- app/contact/contact.tsx
+++ app/contact/contact.tsx
@@ -1,10 +1,10 @@
 
 export default function contact() {
-  return (
-    <div>
-        <h1>
-            This is the contact page
-        </h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>
+                This is the contact page
+            </h1>
+        </div>
+    )
 }
