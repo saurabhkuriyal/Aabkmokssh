d6f86bf9979d59f0999e85586f3f9317105ae1b5
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-12-25 17:09:08 +0530

    adding navbar components to home again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { Header } from "@/components/custom/Header";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-//import "./globals.css";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div>
+          <Header />
         {children}
         </div>
       </body>
