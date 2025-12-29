31e0631accdf9c9007da3e31248722e914e9e5a1
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-12-30 00:13:01 +0530

    modifying Navbar

--- components/custom/DemoOne.tsx
+++ components/custom/DemoOne.tsx
@@ -2,7 +2,8 @@ import GradientMenu from "../gradient-menu";
 
 const DemoOne = () => {
     return (
-        <div className="flex w-full h-screen justify-center items-center">
+        <div className="flex w-[95vw] my-3 mx-5 h-[15vh] px-2 justify-between items-center border rounded-2xl bg-white/20 dark:bg-black/10 backdrop-blur-md border border-white/20 dark:border-black/10 shadow-lg overflow-hidden">
+            <h1>Here will be the logo</h1>
             <GradientMenu />
         </div>
     );
