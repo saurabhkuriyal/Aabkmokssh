25d5269e776db1d674482886b7c93fb59beae940
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-01 22:57:43 +0530

    adding corousel main page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
+import CarouselDemo from "@/components/custom/CarouselDemo";
 
 export default function Home() {
   return (
     <div>
-
+      <CarouselDemo />
       <h1>Here is the homepage</h1>
       <p>what can you do for me</p>
       <pre>here is this for testing</pre>
