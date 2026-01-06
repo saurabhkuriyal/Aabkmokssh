997155b54fa1317e52081bd6c0e4eb1184787b7f
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-06 23:00:39 +0530

    configuring items to main page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import Items from "@/components/custom/Items";
 import CarouselDemo from "@/components/custom/NewCarousel";
 
 export default function Home() {
   return (
     <div>
       <CarouselDemo />
+      <Items/>
     </div>
   );
 }
