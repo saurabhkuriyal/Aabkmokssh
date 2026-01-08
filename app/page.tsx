f91af59f9447fa8646a6c65c606d24514f1abc39
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-08 23:57:13 +0530

    adding 3d card to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import Items from "@/components/custom/Items";
 import CarouselDemo from "@/components/custom/NewCarousel";
+import PremiumCard from "@/components/custom/PremiumCard";
 
 export default function Home() {
   return (
     <div>
       <CarouselDemo />
       <Items/>
+      <PremiumCard/>
     </div>
   );
 }
---
+++ components/custom/PremiumCard.tsx
@@ -0,0 +1,5 @@
+import { Floating3DCard } from "@/components/ui/3d-card";
+
+export default function PremiumCard() {
+    return <Floating3DCard />;
+}
