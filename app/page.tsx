1b72d478e20bf54af1f573ecdcc64d2278e6aac0
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-09 23:13:27 +0530

    applying testimonial to main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Items from "@/components/custom/Items";
 import CarouselDemo from "@/components/custom/NewCarousel";
 import PremiumCard from "@/components/custom/PremiumCard";
+import { AnimatedTestimonialsDemo } from "@/components/custom/Testimonials";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <CarouselDemo />
       <Items/>
       <PremiumCard/>
+      <AnimatedTestimonialsDemo/>
     </div>
   );
 }
