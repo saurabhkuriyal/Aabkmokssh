7b8efab223683aeee872c6683ddc8cf457873e59
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-18 22:50:02 +0530

    improving background

--- components/custom/Items.tsx
+++ components/custom/Items.tsx
@@ -67,17 +67,17 @@ const products = [
 
 export default function Items() {
     return (
-        <div className="bg-white">
+        <div className="bg-gradient-to-b from-white via-pink-100 to-white/30">
             <div className="mx-auto max-w-2xl px-4 py-2 sm:px-6 sm:py-2 lg:max-w-7xl lg:px-8">
                 <h2 className="sr-only">Products</h2>
 
-                <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 ">
                     {products.map((product) => (
                         <a key={product.id} href={product.href} className="group">
                             <img
                                 alt={product.imageAlt}
                                 src={product.imageSrc}
-                                className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
+                                className="aspect-square w-full rounded-lg bg-gray-400 object-cover group-hover:opacity-75 xl:aspect-7/8"
                             />
                             <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
                             <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
