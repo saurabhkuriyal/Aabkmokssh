622fcc1990097e1f87654e6a4bb30ebb137f1121
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-18 23:26:25 +0530

    changing tailind jsx into tsx

--- app/crystal/[id]/page.tsx
+++ app/crystal/[id]/page.tsx
@@ -1,8 +1,11 @@
 
+import Product from "@/components/custom/Product"
+
 export default function page() {
   return (
     <div>
         <h1>Here is the crystal Blog page</h1>
+        <Product/>
     </div>
   )
 }
