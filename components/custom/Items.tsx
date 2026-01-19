db1dbde8958a919046cc3dbba562425376df4eaa
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2026-01-20 00:00:54 +0530

    adding routes to every stone

--- components/custom/Items.tsx
+++ components/custom/Items.tsx
@@ -2,7 +2,7 @@ const products = [
     {
         id: 1,
         name: 'Blue Stone',
-        href: '#',
+        href: '/crystal/1',
         price: 'Rs. 500',
         imageSrc: "/Blue_stone.jpg",
         imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
@@ -10,7 +10,7 @@ const products = [
     {
         id: 2,
         name: 'Emarald',
-        href: '#',
+        href: '/crystal/2',
         price: 'Rs. 1000',
         imageSrc: '/Emaral.jpg',
         imageAlt: 'Olive drab green insulated bottle with flared screw lid and flat top.',
@@ -18,7 +18,7 @@ const products = [
     {
         id: 3,
         name: 'Blue Gem',
-        href: '#',
+        href: '/crystal/3',
         price: 'Rs. 750',
         imageSrc: '/Blue_Rectangular.jpg',
         imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
@@ -26,7 +26,7 @@ const products = [
     {
         id: 4,
         name: 'Green Stone',
-        href: '#',
+        href: '/crystal/4',
         price: 'Rs. 600',
         imageSrc: '/Green_Stone.jpg',
         imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
@@ -34,7 +34,7 @@ const products = [
     {
         id: 5,
         name: 'Red Stone',
-        href: '#',
+        href: '/crystal/5',
         price: 'Rs. 800',
         imageSrc: '/curr_hero.jpg',
         imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
@@ -42,7 +42,7 @@ const products = [
     {
         id: 6,
         name: 'Emarald Gemstone',
-        href: '#',
+        href: '/crystal/6',
         price: 'Rs. 1200',
         imageSrc: '/Emarald.avif',
         imageAlt: 'Stack of 3 small drab green cardboard paper card refill boxes with white text.',
@@ -50,7 +50,7 @@ const products = [
     {
         id: 7,
         name: 'Dark Blue Gemstone',
-        href: '#',
+        href: '/crystal/7',
         price: 'Rs. 700',
         imageSrc: '/Blue_Rectangular.jpg',
         imageAlt: 'Brass scissors with geometric design, black steel finger holes, and included upright brass stand.',
@@ -58,7 +58,7 @@ const products = [
     {
         id: 8,
         name: "Green Gemstone",
-        href: '#',
+        href: '/crystal/8',
         price: 'Rs. 650',
         imageSrc: '/Green_Stone.jpg',
         imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
